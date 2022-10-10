f197097cb0d58cbf93dfaec2f9d525080f5c3641
Muhammad Usman <usman.gondal238@gmail.com>
2022-10-11 01:31:07 +0500

    initial commit

---
+++ animal.py
@@ -0,0 +1,25 @@
+import sys
+
+def cat():
+    print('Meow!')
+def default():
+     print('Hello')
+
+def dog():
+    print('woof!')
+
+def main():
+<<<<<<< HEAD
+    if sys.argv[1] == 'cat': 
+        cat()
+    else:
+         default()
+=======
+    if sys.argv[1] == 'dog':
+        dog()
+    else:        
+        default()
+>>>>>>> dog
+
+if __name__ == '__main__':
+    main()
